6c60b908f566e656b2d329f3e6329e47374988a5
daepstein@gmail.com <daepstein@gmail.com>
2018-12-25 18:53:48 +0000

    atualizando

--- newbash.sh
+++ newbash.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 while inotifywait -q -e modify /home/bitnami/htdocs/config-repo/config-repo/mining-development.yml>/dev/null; do
-    echo "filename is changed"
+        echo "$(date) $line";
+
+    echo "filename is changed".
 echo  "Atualizando Github";
 git add .;
 git commit -m "atualizando";
 git push;
+ curl -X POST http://gomining-env.uppk4f29t3.sa-east-1.elasticbeanstalk.com/actuator/refresh
     # do whatever else you need to do
 done
